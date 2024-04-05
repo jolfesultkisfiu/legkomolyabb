dd766f5b9399f72bca8ededa5183f6db395f3a24
RobertNagy299 <nagyrobertoffical@gmail.com>
2024-04-05 19:16:21 +0200

    solved the form thing

--- projekt/php/createevent.php
+++ projekt/php/createevent.php
@@ -1,13 +1,19 @@
 <?php
     
     require_once "eventclass.php";
+  
+
+    
     if (isset($_POST["submitBT"])) {
+       print_r( $_FILES["eventThumbnail"]);
+      
     if(
         isset($_POST["eventTitle"]) && isset($_POST["eventDetails"])
         &&  isset($_POST["eventDate"]) && 
         (isset($_POST["mobileUserEventTime"]) || (isset($_POST["selectHour"]) && isset($_POST["selectMinute"])) )
-        && isset($_POST["eventThumbnail"]) && isset($_POST["eventLocation"])       
+        && isset($_FILES["eventThumbnail"]) && isset($_POST["eventLocation"])       
     ){
+        
         $title = trim($_POST["eventTitle"]);
         $details = trim($_POST["eventDetails"]);
         $date = $_POST["eventDate"];
@@ -18,11 +24,18 @@
         else {
             $startingTime = $_POST["selectHour"] . $_POST["selectMinute"];
         }
-        $thumbnail = $_POST["eventThumbnail"];
+        $thumbnail = $_FILES["eventThumbnail"];
         $location = trim($_POST["eventLocation"]);
 
         $event = new Event($title, $details, $date, $startingTime, $thumbnail, $location);
-        print_r($event);
+       // print_r($event);
+       
+       $aExtraInfo = getimagesize($_FILES['eventThumbnail']['tmp_name']);
+       $sImage = "data:" . $aExtraInfo["mime"] . ";base64," . base64_encode(file_get_contents($_FILES['eventThumbnail']['tmp_name']));
+       echo '<p>The image has been uploaded successfully</p><p>Preview:</p><img src="' . $sImage . '" alt="Your Image" />';
+
+       $jsondata = json_encode($event, JSON_PRETTY_PRINT);
+       file_put_contents("../json/events.json",$jsondata);
 
     }}
 
